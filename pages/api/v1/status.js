fc035e4871908927b5aaf8ccc26a63541cd9799a
Eric Augusto <149190070+EricAugust2006@users.noreply.github.com>
2026-04-28 01:19:17 +0000

    add  and  files

---
+++ pages/api/v1/status.js
@@ -0,0 +1,6 @@
+import database from "../../../infra/database.js";
+
+export default async function status(request, response) {
+  const result = await database.query("SELECT 1 + 1;");
+  response.status(200).json({ chave: "são acima da média" });
+}
