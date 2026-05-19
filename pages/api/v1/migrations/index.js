961a369fee5b0691bb97ebf25872031e31592e9a
Eric Augusto <149190070+EricAugust2006@users.noreply.github.com>
2026-05-19 03:21:20 +0000

    estou mt cansado pra pensar em commit

--- pages/api/v1/migrations/index.js
+++ pages/api/v1/migrations/index.js
@@ -1,9 +1,32 @@
-import migrationRunner from "node-pg-migrate";
+import { join } from "node:path";
+const { runner: migrationRunner } = require("node-pg-migrate");
 
 export default async function migrations(request, response) {
-  const migrations = await migrationRunner({
-    databaseUrl: process.env.DATABASE_URL,
-    dryRun: true,
-  });
-  response.status(200).json([migrations]);
+  if (request.method === "GET") {
+    const migrations = await migrationRunner({
+      databaseUrl: process.env.DATABASE_URL,
+      dryRun: true,
+      dir: join(process.cwd(), "infra", "migrations"),
+      direction: "up",
+      verbose: true,
+      migrationsTable: "pgmigrations",
+    });
+    response.status(200).json(migrations);
+    console.log("Entrou no GET");
+  }
+
+  if (request.method === "POST") {
+    const migrations = await migrationRunner({
+      databaseUrl: process.env.DATABASE_URL,
+      dryRun: false,
+      dir: join(process.cwd(), "infra", "migrations"),
+      direction: "up",
+      verbose: true,
+      migrationsTable: "pgmigrations",
+    });
+    response.status(200).json(migrations);
+    console.log("Entrou no POST");
+  }
+
+  return response.status(405).end;
 }
